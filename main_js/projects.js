f7662ef934874d13f539b505fab7a0c3e752c3e0
Eric Samson <53947969+EricSamsonCarto@users.noreply.github.com>
2022-10-21 10:10:23 -0700

    typo

--- main_js/projects.js
+++ main_js/projects.js
@@ -29,9 +29,9 @@ $('#projects_section').load('assets/projects.html', function() {
 
     //initial load page
     var lower_row_page_num = 0;
-    var row_length = 2;
+    var upper_row_page_num = row_length;
     //display the first two rows for the first page
-    $(".project_row").slice(lower_row_page_num, row_length).show();
+    $(".project_row").slice(lower_row_page_num, upper_row_page_num).show();
 
     //on next page click
     $(".next_page").click(function(){
@@ -39,11 +39,11 @@ $('#projects_section').load('assets/projects.html', function() {
             return;
         }
         else {
-            $(".project_row").slice(lower_row_page_num, row_length).hide();
+            $(".project_row").slice(lower_row_page_num, upper_row_page_num).hide();
             lower_row_page_num += 2;
-            row_length += 2;
-            $(".project_row").slice(lower_row_page_num, row_length).show();
-                if ($(".project_row").length == row_length) {
+            upper_row_page_num += 2;
+            $(".project_row").slice(lower_row_page_num, upper_row_page_num).show();
+                if ($(".project_row").length == upper_row_page_num) {
                     $('.next-btn').prop("disabled", true);
                 }
                 if (lower_row_page_num != 0) {
@@ -58,11 +58,11 @@ $('#projects_section').load('assets/projects.html', function() {
             return;
         }
         else {
-            $(".project_row").slice(lower_row_page_num, row_length).hide();
+            $(".project_row").slice(lower_row_page_num, upper_row_page_num).hide();
             lower_row_page_num -= 2;
-            row_length -= 2;
-            $(".project_row").slice(lower_row_page_num, row_length).show();
-                if ($(".project_row").length != row_length) {
+            upper_row_page_num -= 2;
+            $(".project_row").slice(lower_row_page_num, upper_row_page_num).show();
+                if ($(".project_row").length != upper_row_page_num) {
                     $('.next-btn').prop("disabled", false);
                 }
                 if (lower_row_page_num === 0) {
